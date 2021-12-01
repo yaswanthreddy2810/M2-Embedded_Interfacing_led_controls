819ebc9684950e145c65c23c66e8fb7c3417e112
yaswanthreddy2810 <85639101+yaswanthreddy2810@users.noreply.github.com>
2021-12-01 23:08:31 +0530

    Update led_on.c

--- 3_Implementation/src/led_on.c
+++ 3_Implementation/src/led_on.c
@@ -1,1 +1,20 @@
+#include "led_control.h"
 
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+void led_on()
+{
+    PORTB |=(1<<PORTB2);
+		PORTB &=~(1<<PORTB3);
+		_delay_ms(2000);
+		
+		PORTB &=~(1<<PORTB2);
+		_delay_ms(4000);
+		PORTB |=(1<<PORTB3);
+		_delay_ms(2000);
+
+}
